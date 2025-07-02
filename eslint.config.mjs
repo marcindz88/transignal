1191ecb9cde79e54a96d65e518eb7ce01a6061ce
marcindz88 <marcindz88@gmail.com>
2025-07-02 09:07:32 +0200

    Configure eslint to handle and optimize imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,8 @@ import json from '@eslint/json';
 import css from '@eslint/css';
 import { defineConfig } from 'eslint/config';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import unusedImports from 'eslint-plugin-unused-imports';
+import importPlugin from 'eslint-plugin-import';
 
 export default defineConfig([
   {
@@ -32,10 +34,51 @@ export default defineConfig([
     language: 'css/css',
     extends: ['css/recommended'],
   },
-  eslintPluginPrettierRecommended,
   {
+    files: ['**/*.ts', '**/*.js'],
+    extends: [importPlugin.flatConfigs.recommended],
+    plugins: {
+      'unused-imports': unusedImports,
+    },
     rules: {
+      'no-console': 'error',
+      'no-debugger': 'error',
+      'import/first': [2],
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      'import/no-unresolved': 'off',
+      'import/named': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      'object-property-newline': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'import/order': [
+        'error',
+        {
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: false,
+          },
+
+          groups: ['builtin', 'external', 'index', 'object', 'internal', ['sibling', 'parent']],
+        },
+      ],
     },
   },
+  eslintPluginPrettierRecommended,
 ]);
